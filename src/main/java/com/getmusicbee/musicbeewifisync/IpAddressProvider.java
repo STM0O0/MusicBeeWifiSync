7445a112a93a057ba829290b9ed648624e9abef5
STM0O0 <stm@live.be>
2023-04-03 20:12:54 +0200

    mistake

--- src/main/java/com/getmusicbee/musicbeewifisync/IpAddressProvider.java
+++
@@ -1,9 +0,0 @@
-package com.getmusicbee.musicbeewifisync;
-
-import java.net.InetAddress;
-import java.util.Iterator;
-
-public interface IpAddressProvider extends Iterable<InetAddress>, Iterator<InetAddress> {
-    InetAddress getDeviceAddress();
-    String getIpSearchPrefix();
-}
